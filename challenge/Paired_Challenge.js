24d88f72ef866f305db47c390e714057dd7da448
Ross Landino <mrlandino@gmail.com>
2022-01-12 20:16:05 -0700

    Initial Commit

---
+++ challenge/Paired_Challenge.js
@@ -0,0 +1,8 @@
+//quick name didnt relate to subject
+var mainCharacter = 'Harry';
+var wizard = true;
+var year = 3;
+var house = 'Gryffindor';
+var favTeacher = 'Dumbeldoor';
+var bestFriends = 2;
+var slytherinStudents = ['Malfoy', 'Crab', 'Goyle'];
